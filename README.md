26f27ba968bdce243d1ba6e60346d13f85f1b28e
Alexander Kuemmel <akisys@users.noreply.github.com>
2022-12-06 10:24:37 +0100

    ADD: initial version, minimal logger for pipelines

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# az_application_insights_logger
-Push traces and events to Azure Application Insights
+# Azure Application Insights logger
+
+Push traces and events to Azure Application Insights.
+
+> Note: this is a minimal Python-based implementation, which is intended to be packaged as single binary.
+
+## Configuration
+
+- configuration file (YAML-based) is used for Python logging framework config and message envelope overrides.
+- cli arguments are used for partial message envelop overrides
+
+> Example config file
+```
+message_envelope:
+  tags:
+    "ai.cloud.role": "app"
+    "ai.cloud.roleInstance": "pipeline1"
+    "ai.operation.parentId": "job1234"
+    "ai.operation.id": "step123456"
+    "ai.operation.name": "step name"
+    # "ai.device.id": "bot1"
+    # "ai.device.locale": "en_US"
+    # "ai.device.type": "logbot"
+    # "ai.device.model": "pipeline bot"
+    # "ai.device.osVersion": "some other linux"
+logging_config:
+  version: 1
+  formatters:
+    simple:
+      format: '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    super_simple:
+      format: '%(name)s - %(levelname)s - %(message)s'
+  handlers:
+    console:
+      class: logging.StreamHandler
+      level: DEBUG
+      formatter: simple
+      stream: ext://sys.stdout
+    event:
+      class: opencensus.ext.azure.log_exporter.AzureEventHandler
+      formatter: super_simple
+    trace:
+      class: opencensus.ext.azure.log_exporter.AzureLogHandler
+      formatter: super_simple
+  loggers:
+    aitracelog:
+      level: DEBUG
+      handlers: [trace]
+      propagate: no
+    aieventlog:
+      level: DEBUG
+      handlers: [event]
+      propagate: no
+  root:
+    handlers: [console]
+```
+
+## Usage
+
+```
+Usage: appinsights_logger.py [OPTIONS]
+
+Options:
+  -c PATH                         Configuration file path.  [default:
+                                  appinsights_logconfig.yml]
+  -a TEXT                         Application Insights Logger instance name.
+                                  Needs to coincide within logger config!
+                                  [default: aipusher]
+  -l [critical|error|warning|info|debug]
+                                  Log Level for submitted message.  [default:
+                                  info]
+  -m TEXT                         Log Message to submit.  [required]
+  -t TEXT                         Override tags, check dist config for
+                                  available values.  [default:
+                                  ai.cloud.role=aipusher]
+  -v                              Verbosity level, increased by multiple use.
+  --help                          Show this message and exit.
+```
+
+## Single binary packaging
+
+- requirements: ccache, gcc, patchelf
+
+- test first with:
+  ```
+  nuitka3 appinsights_logger.py --static-libpython=yes --standalone
+  ```
+  - execute binary within the `dist` folder
+
+- if previous test works, try with:
+  ```
+  nuitka3 appinsights_logger.py --static-libpython=yes --onefile
+  ```
---
+++ appinsights_logger.py
@@ -0,0 +1,156 @@
+"""
+Azure Application Insights Logger
+"""
+
+"""
+Copyright 2022 Alexander Kuemmel
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import sys
+import yaml
+import typer
+import logging
+import logging.config
+from enum import Enum
+from opencensus.ext.azure.log_exporter import (
+    AzureLogHandler,
+    AzureEventHandler,
+    Envelope,
+)
+from pathlib import Path
+from typing import Optional, List
+from pprint import pformat
+
+logging.basicConfig(level=logging.ERROR)
+int_log: logging.Logger = None
+ai_logger: logging.Logger = None
+ai_logger_name: str = os.getenv("APPINSIGHTS_LOGGER_NAME") or sys.argv[0]
+app_config: dict = {}
+
+
+class LogLevel(str, Enum):
+    critical = "critical"
+    error = "error"
+    warning = "warning"
+    info = "info"
+    debug = "debug"
+
+
+def telemetry_processor(envelope: Envelope) -> bool:
+    int_log.debug("Executing Application Insights telemetry processor")
+    msg_wrap = app_config.get("message_envelope")
+    if msg_wrap is None:
+        int_log.debug("No message wrap found in config, skipping processor.")
+        return True
+    msg_wrap_tags = msg_wrap.get("tags")
+    if msg_wrap_tags is not None:
+        int_log.debug("Merging config tags override into envelope.")
+        envelope.tags.update(msg_wrap_tags)
+    int_log.debug("Final message envelope\n" + pformat(envelope, indent=2))
+    return True
+
+
+def init_ai_logger(app_name: str, config: dict):
+    global ai_logger
+    logging.config.dictConfig(config)
+    ai_logger = logging.getLogger(app_name)
+
+    if ai_logger.hasHandlers():
+
+        event_handlers = [h for h in ai_logger.handlers if h.name == "event"]
+        if len(event_handlers) == 1:
+            event_handler: AzureEventHandler = event_handlers.pop()
+            event_handler.add_telemetry_processor(telemetry_processor)
+        elif len(event_handlers) > 1:
+            raise RuntimeError("More than one Application Insights Event handler found")
+
+        trace_handlers = [h for h in ai_logger.handlers if h.name == "trace"]
+        if len(trace_handlers) == 1:
+            trace_handler: AzureLogHandler = trace_handlers.pop()
+            trace_handler.add_telemetry_processor(telemetry_processor)
+        elif len(trace_handlers) > 1:
+            raise RuntimeError("More than one Application Insights Trace handler found")
+
+
+def main(
+    config: Path = typer.Option(
+        Path("appinsights_logconfig.yml"),
+        "-c",
+        readable=True,
+        help="Configuration file path.",
+    ),
+    ai_logger_name: str = typer.Option(
+        ai_logger_name, "-a", help="Application Insights Logger instance name. Needs to coincide within logger config!"
+    ),
+    level: LogLevel = typer.Option(
+        "info", "-l", help="Log Level for submitted message."
+    ),
+    msg: Optional[str] = typer.Option(..., "-m", help="Log Message to submit."),
+    tag: Optional[List[str]] = typer.Option(
+        [f"ai.cloud.role={ai_logger_name}"],
+        "-t",
+        help="Override tags, check dist config for available values.",
+    ),
+    verbosity: int = typer.Option(0, "-v", count=True, show_default=False, help="Verbosity level, increased by multiple use."),
+):
+    global int_log, app_config
+    app_name = ai_logger_name
+    int_log = logging.getLogger()
+    app_config = yaml.safe_load(config.read_text())
+    init_ai_logger(app_name=app_name, config=app_config.get("logging_config"))
+
+    if tag is not None:
+        tag.sort()
+        tags_override = {}
+        for label in tag:
+            tag_parts = label.split("=")
+            if len(tag_parts) == 1:
+                continue
+            tags_override.update({tag_parts[0]: tag_parts[1]})
+        int_log.debug("Updating tags with overrides in app config.")
+        msg_env = app_config.get("message_envelope", {})
+        msg_env_tags = msg_env.get("tags", {})
+        msg_env_tags.update(tags_override)
+        msg_env.update(dict(tags=msg_env_tags))
+        app_config.update(dict(message_envelope=msg_env))
+
+    if verbosity > 2:
+        int_log.setLevel(logging.DEBUG)
+    elif verbosity > 1:
+        int_log.setLevel(logging.INFO)
+    elif verbosity > 0:
+        int_log.setLevel(logging.WARNING)
+
+    int_log.info("Logger message processing")
+    if msg is None:
+        sys.exit(0)
+    ai_log = ai_logger.debug
+    if level is LogLevel.info:
+        ai_log = ai_logger.info
+    elif level is LogLevel.warning:
+        ai_log = ai_logger.warning
+    elif level is LogLevel.critical:
+        ai_log = ai_logger.critical
+    elif level is LogLevel.error:
+        ai_log = ai_logger.error
+
+    ai_log(msg)
+
+    int_log.info("Logger done")
+
+
+if __name__ == "__main__":
+    typer.run(main)
